a8b15922b391fe322cad04f12503760de316090c
mykyta-boichuk <50881562+mykyta-boichuk@users.noreply.github.com>
2021-11-11 23:20:00 +0200

    first commit

---
+++ src/componets/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+function App () {
+  return (
+    <div className={styles.app}>
+
+    </div>
+  );
+}
+
+export default App;
